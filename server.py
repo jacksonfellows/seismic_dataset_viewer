f14395c7d41fc2572a9e5603843e1956a1ed8598
Jackson Fellows <jtfellows@icloud.com>
2024-02-07 10:00:28 -0500

    Add "homepage" w/ list of downloaded events.

--- server.py
+++ server.py
@@ -22,3 +22,16 @@ def xy(event_id, channel):
     response = flask.make_response(XY.tobytes())
     response.headers.set("Content-Type", "application/octet-stream")
     return response
+
+@app.route("/")
+def index():
+    event_info = {}
+    for event_dir in Path("events").iterdir():
+        event_id = event_dir.name
+        with open(event_dir / "metadata.json", "r") as f:
+            event_m = json.load(f)
+            event_info[event_id] = {
+                "reference_channel": event_m["reference_channel"],
+                "trace_start_time": event_m["trace_start_time"]
+            }
+    return flask.render_template("home.html", event_info=event_info)
